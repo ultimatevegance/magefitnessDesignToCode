0f547917f739252b66f1bfefd893e20ca5247af3
xiongyongjie <xiongyj@dxy.net>
2019-09-12 14:46:55 +0800

    project init

---
+++ magefitness/magefitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magefitness
+//
+//  Created by xiongyongjie on 2019/9/12.
+//  Copyright © 2019 MonsterTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
